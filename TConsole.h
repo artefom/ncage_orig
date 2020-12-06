75e29ebc99effa2808c5e860d9a2d111ef1aee58
Artyom Fomenko <aofomenko@gmail.com>
2020-12-06 21:46:51 +0300

    Applied stash back

--- TConsole.h
+++ TConsole.h
@@ -1,194 +1,266 @@
-class TConsole_IContainter {
-public:
-    virtual void Call(IArguments *) = 0;
-};
+const unsigned short TC_ARG_NONE = 0;
+const unsigned short TC_ARG_BOOL = 1;
+const unsigned short TC_ARG_INT = 2;
+const unsigned short TC_ARG_FLOAT = 3;
+const unsigned short TC_ARG_CHAR = 4;
+const unsigned short TC_ARG_COLOR = 5;
 
-template<class F>
-class TConsole_Container : public TConsole_IContainter {
-};
+#include <TConsole_variables.h>
+#include <TConsole_functions.h>
 
-template<>
-class TConsole_Container<void (*)(void)> : public TConsole_IContainter // без аргументов
-{
-public:
-
-    typedef void (*M)(void);
-
-    M f;
-
-    TConsole_Container(M func, unsigned char &arg_type) {
-        f = func;
-        arg_type = 0;
-    }
-
-    virtual void Call(IArguments *args) {
-        f();
-    };
-};
-
-template<class A1>
-class TConsole_Container<void (*)(A1)> : public TConsole_IContainter // один аргумент
-{
+class TC_Ptr {
 public:
 
-    typedef void (*M)(A1);
-
-    typedef Arguments <A1> A;
-    M f;
-
-    TConsole_Container(M func, unsigned char &arg_type) {
-        f = func;
-
-        if (typeid(A1) == typeid(int)) {
-            arg_type = 1;
-            return;
-        };
-        if (typeid(A1) == typeid(float)) {
-            arg_type = 2;
-            return;
-        };
-        if (typeid(A1) == typeid(char *)) {
-            arg_type = 4;
-            return;
-        };
-    }
-
-    virtual void Call(IArguments *i_args) {
-        A *args = dynamic_cast<A *>(i_args);
-        f(args->arg1);
-    };
-};
-
-
-class TConsole_Callback {
-private:
-    TConsole_IContainter *func_container;
-public:
     char *name;
-    unsigned char arg_type;
+    void *ptr;
+    unsigned char type; // 0 - variable 1 - function
 
-    TConsole_Callback() {
-        name = 0;
-        arg_type = 0;
-        func_container = 0;
-    }
+    TC_Ptr() : name(0), ptr(0), type(0) {};
 
-    ~TConsole_Callback() {
-        if (name) delete[] name;
-        if (func_container) delete func_container;
-    }
-
-    template<class F>
-    void Connect(F f) {
-        if (func_container) delete func_container;
-        func_container = new TConsole_Container<F>(f, arg_type);
-
-        cout << "registering func with arguments type: " << (unsigned int) arg_type << endl;
-    }
-
-    virtual void operator()(IArguments *) {
-        func_container->Call((Arguments<> * )(0));
-    }
-
-    template<class T1>
-    void operator()(T1 i_arg1) {
-
-        Arguments <T1> args(i_arg1);
-        func_container->Call(&args);
-    }
+    TC_Ptr(char *iname, void *iptr, unsigned char itype) : name(iname), ptr(iptr), type(itype) {};
 };
 
-vector <TConsole_Callback> console_functions;
+class TC_Universal {
+public:
+    I_TConsole_Var *var;
+    TConsole_Callback *cback;
 
+    TC_Universal() {
+        var = 0;
+        cback = 0;
+    };
 
-void testfunction_int(int a) {
-    cout << "test function called with int rgument: " << a << endl;
-}
+    TC_Universal(I_TConsole_Var *ivar) {
+        var = ivar;
+        cback = 0;
+    };
 
-void testfunction_float(float a) {
-    cout << "test function called with float argument: " << a << endl;
-}
-
-void testfunction_string(char *a) {
-    cout << "test function called with string argument: " << a << endl;
-}
-
-void testfunction_noargs() {
-    cout << "test function called with no args " << endl;
-}
-
-int getnum(char *text) {
-    int output = 0;
-    while ((*text != 0) && !(((*text) >= '0') && ((*text) <= '9'))) ++text;
-    while ((*text != 0) && (((*text) >= '0') && ((*text) <= '9'))) {
-        output = output * 10 + (*text) - '0';
-        ++text;
-    }
-    return output;
-}
-
-bool syntax_analyze(char *text, int &func_id, int &arg) {
-    unsigned int j = 0;
-    for (unsigned int i = 0; i != console_functions.size(); ++i) {
-        for (j = 0; (console_functions[i].name[j] != 0) &&
-                    (console_functions[i].name[j] == text[j]); ++j);
-        if ((console_functions[i].name[j] == 0) && (isspace(text[j]) || (text[j] == 0))) {
-            func_id = i;
-            goto end;
-        }
-    }
-    return false;
-    end:;
-
-    int ai = 0;
-    float fi = 0;
-    char *ci = 0;
-
-    cout << "found function! " << endl;
-    cout << "console_functions[func_id].arg_type = " << (unsigned int) console_functions[func_id].arg_type << endl;
-    switch (console_functions[func_id].arg_type) {
-        case 1 :
-            sscanf(text + j, "%d", &ai);
-            cout << "calling!" << endl;
-            console_functions[func_id](ai);
-            break;
-        case 2 :
-            sscanf(text + j, "%f", &fi);
-            console_functions[func_id](fi);
-            break;
-        case 4 :
-            sscanf(text + j, "%s", &ci);
-            if ((ci != 0) && (*ci != 0)) console_functions[func_id](ci);
-            break;
-        default:
-            console_functions[func_id](0);
-            break;
-    }
-
-    return true;
-
+    TC_Universal(TConsole_Callback *icback) {
+        cback = icback;
+        var = 0;
+    };
 };
 
 class TConsole {
 public:
 
+    map <string, TC_Universal> functions;
+    vector<const char *> functions_names;
+    vector<string *> lines;
+    unsigned int max_lines_number = 256;
+
     TConsole() {
-        console_function_register(&testfunction_int, "function_test_int");
-        console_function_register(&testfunction_float, "function_test_float");
-        console_function_register(&testfunction_string, "function_test_string");
-        console_function_register(&testfunction_noargs, "function_test_noargs");
+        lines.push_back(new string());
     }
 
-    void EnterText(char *text) {
-        ProcessCommand(text);
+
+    void ProcessCommandsFromFile(const char *text) {
+        string STRING;
+        ifstream infile;
+        infile.open(text);
+        while (!infile.eof()) {
+            getline(infile, STRING);
+            ProcessCommand(STRING, 0);
+        }
+        infile.close();
     };
 
-    void ProcessCommand(char *command) {
-        int id, arg;
-        syntax_analyze(command, id, arg);
+    bool ProcessCommand(string cmd, bool annotations) {
+        char command[256];
+        command[0] = 0;
+        char parameter[256];
+        parameter[0] = 0;
+        sscanf(cmd.c_str(), "%255s%*[ ]%255[^\n]", command, parameter);
+        command[255] = 0;
+        parameter[255] = 0;
+        map<string, TC_Universal>::iterator it = functions.find(command);
+        if (it == functions.end()) return false;
+        I_TConsole_Var *v = it->second.var;
+        TConsole_Callback *fv = it->second.cback;
 
-        //if (
+        if (v) {
+            if ((parameter[0] == 0) && annotations) {
+                char txt[64];
+                v->snprintf(txt, 64, v->val_ptr);
+                (*this) << v->name << " = " << txt;
+                v->snprintf(txt, 64, v->def_ptr);
+                (*this) << " (default: " << txt << ")" << endl;
+            } else v->sscanf(parameter);
+            return true;
+        }
+
+        if (fv) {
+            if (parameter[0] == 0) {
+                int iarg = (int) (fv->arg_type);
+                if (iarg == TC_ARG_NONE) { fv->sscanf_call(parameter); };
+                if (annotations) {
+
+                    switch (iarg) {
+                        case TC_ARG_NONE :
+                            break;
+                        case TC_ARG_INT :
+                            (*this) << " int";
+                            break;
+                        case TC_ARG_FLOAT :
+                            (*this) << " float";
+                            break;
+                        case TC_ARG_CHAR :
+                            (*this) << " string";
+                            break;
+                        case TC_ARG_COLOR :
+                            (*this) << " color";
+                            break;
+                        case TC_ARG_BOOL :
+                            (*this) << " bool";
+                            break;
+                        default:
+                            (*this) << " unknown";
+                    }
+                    if (fv->annotations) (*this) << ' ' << '"' << fv->annotations << '"';
+                    if (fv->annotations || iarg != TC_ARG_NONE) (*this) << endl;
+                };
+            } else {
+                fv->sscanf_call(parameter);
+            }
+            return true;
+        }
+        return false;
+    };
+
+    template<class T>
+    T &RegisterVar(T def, const char *name, void (*func)(void) = 0) {
+        TConsole_Var <T> *var = new TConsole_Var<T>(def, name, func);
+        functions[name] = TC_Universal(var);
+        return var->value;
+    };
+
+    template<class F>
+    void RegisterFunction(F f, const char *name, const char *annotations = 0) {
+        TConsole_Callback *func = new TConsole_Callback();
+        func->Connect(f);
+        func->name = name;
+        func->annotations = annotations;
+        functions[name] = TC_Universal(func);
     }
+
+    void EnterText(string text) {
+        AddText(text);
+        flush();
+        ProcessCommand(text, true);
+    };
+
+    void AddText(string text) {
+        size_t found_last = 0;
+        size_t found = text.find_first_of('\n');
+        while (found != string::npos) {
+            *lines.back() += text.substr(found_last, found - found_last);
+            lines.push_back(new string());
+            found_last = found + 1;
+            found = text.find_first_of('\n', found + 1);
+        }
+        *lines.back() += text.substr(found_last);
+        ui_events.Call(&TUI_Childless::Global_Hooks_Storage, HOOK_GC_OnEnterText, text.c_str());
+    }
+
+    void flush() {
+        AddText("\n");
+    };
+
+    template<class T>
+    TConsole &operator<<(T in) {
+        stringstream ss;
+        ss << in;
+        string st = ss.str();
+        AddText(st);
+        return *this;
+    }
+
+    typedef std::basic_ostream<char, std::char_traits<char> > CoutType;
+
+    typedef CoutType &(*StandardEndLine)(CoutType &);
+
+    TConsole &operator<<(StandardEndLine manip) {
+        flush();
+        return *this;
+    }
+
+    void Init() {
+        functions_names.reserve(functions.size());
+        for (auto it = functions.begin(); it != functions.end(); ++it) {
+            functions_names.push_back(it->first.c_str());
+        };
+    };
+
 };
 
+char endl(TConsole &os);
+
 TConsole Console;
+
+void testfunction_int(int a) {
+    Console << "test function called with int rgument: " << a << endl;
+}
+
+void testfunction_float(float a) {
+    Console << "test function called with float argument: " << a << endl;
+}
+
+void testfunction_string(const char *a) {
+    Console << "test function called with string argument: " << a << endl;
+}
+
+void testfunction_noargs() {
+    Console << "test function called with no args " << endl;
+}
+
+int TDebug_initialized = 0;
+
+void Exit() {
+    TDebug_initialized = 0;
+    glutLeaveMainLoop();
+};
+
+#define ConsoleRegisterFunction(a, b)    \
+namespace namespace##a{    \
+    static char o = (Console.RegisterFunction( &a, #a, b ),0);    \
+};
+
+#define ConsoleRegisterFunctionCustomName(a, c, b)    \
+namespace namespace##a{    \
+    static char o = (Console.RegisterFunction( &a, c, b ),0);    \
+};
+
+
+ConsoleRegisterFunction(testfunction_int, "just a test function");
+ConsoleRegisterFunction(testfunction_float, "just a test function");
+ConsoleRegisterFunction(testfunction_string, "just a test function");
+ConsoleRegisterFunction(testfunction_noargs, 0);
+ConsoleRegisterFunctionCustomName(Exit, "exit", 0);
+
+void console_printcontent() {
+    cout << "##########Flushing console content: " << endl;
+    int i = 0;
+    for (auto &v : Console.lines) {
+        cout << i << ": " << v << endl;
+        ++i;
+    };
+};
+
+ConsoleRegisterFunction(console_printcontent, "flushing console content");
+
+/*
+STATIC_EXECUTE{
+	Console.RegisterFunction( &testfunction_int, "function_test_int", "just a test function" );
+	Console.RegisterFunction( &testfunction_float, "function_test_float", "just a test function" );
+	Console.RegisterFunction( &testfunction_string, "function_test_string", "just a test function" );
+	Console.RegisterFunction( &testfunction_noargs, "function_test_noargs", "just a test function" );
+}
+*/
+void Test_valSet() {
+    Console << "test_val value changed!" << endl;
+}
+
+int &test_val = Console.RegisterVar(314, "test_val", &Test_valSet);
+
+#include <TConsole_scripts.h>
+
--- UIEvents.h
+++ UIEvents.h
@@ -2,26 +2,40 @@
 
 class IUIContainer {
 public:
-    virtual void Call(list<TUI_Childless *> *a, IArguments *) = 0;
+    virtual void Call(int ID, list<TUI_Childless *> *a, IArguments *) = 0;
 
-    virtual void Call(TUI_Childless *b, IArguments *i_args) = 0;
+    virtual void Call(int ID, TUI_Childless *b, IArguments *i_args) = 0;
 
-    virtual void Call(list<TUI_Childless *> *a, IArguments *, typename list<TUI_Childless *>::iterator &it) = 0;
+    virtual void Call(int ID, list<TUI_Childless *> *a, IArguments *, typename list<TUI_Childless *>::iterator &it) = 0;
 
-    virtual void Call(vector<TUI_Childless *> *a, IArguments *) = 0;
+    virtual void Call(int ID, vector<TUI_Childless *> *a, IArguments *) = 0;
 
-    virtual void Call(vector<TUI_Childless *> *a, IArguments *, typename vector<TUI_Childless *>::iterator &it) = 0;
+    virtual void
+    Call(int ID, vector<TUI_Childless *> *a, IArguments *, typename vector<TUI_Childless *>::iterator &it) = 0;
+
+
+    virtual void SingleCall(int ID, list<TUI_Childless *> *a, IArguments *) = 0;
+
+    virtual void SingleCall(int ID, TUI_Childless *b, IArguments *i_args) = 0;
+
+    virtual void
+    SingleCall(int ID, list<TUI_Childless *> *a, IArguments *, typename list<TUI_Childless *>::iterator &it) = 0;
+
+    virtual void SingleCall(int ID, vector<TUI_Childless *> *a, IArguments *) = 0;
+
+    virtual void
+    SingleCall(int ID, vector<TUI_Childless *> *a, IArguments *, typename vector<TUI_Childless *>::iterator &it) = 0;
 };
 
-template<class B, class T, class M>
+template<class T, class M>
 class UIContainer : public IUIContainer {
 };
 
 // Здесь будет храниться информация о событии
 
 //  Спецификация без аргументов.
-template<class B, class T>
-class UIContainer<B, T, void (T::*)(void)> : public IUIContainer {
+template<class T>
+class UIContainer<T, void (T::*)(void)> : public IUIContainer {
     typedef void (T::*M)(void);
 
 public:
@@ -30,35 +44,86 @@ public:
 
     UIContainer(T *c, M m) : m_class(c), m_method(m) {}
 
-    void Call(list <B> *a, IArguments *i_args) {
+    void Call(int ID, list<TUI_Childless *> *a, IArguments *i_args) {
         for (auto it = a->begin(); it != a->end(); it++) {
             T *c = dynamic_cast<T *>(*it);
-            if (c) { (c->*m_method)(); };
+            if (c) {
+                (c->*m_method)();
+                c->sevents.Call(ID);
+            };
         }
     }
 
-    void Call(list <B> *a, IArguments *i_args, typename list<B>::iterator &it) {
+    void Call(int ID, list<TUI_Childless *> *a, IArguments *i_args, typename list<TUI_Childless *>::iterator &it) {
         for (it = a->begin(); it != a->end(); it++) {
             T *c = dynamic_cast<T *>(*it);
-            if (c) { (c->*m_method)(); };
+            if (c) {
+                (c->*m_method)();
+                c->sevents.Call(ID);
+            };
         }
     }
 
-    void Call(vector <B> *a, IArguments *i_args) {
+    void Call(int ID, vector<TUI_Childless *> *a, IArguments *i_args) {
         for (auto it = a->begin(); it != a->end(); it++) {
             T *c = dynamic_cast<T *>(*it);
-            if (c) { (c->*m_method)(); };
+            if (c) {
+                (c->*m_method)();
+                c->sevents.Call(ID);
+            };
         }
     }
 
-    void Call(vector <B> *a, IArguments *i_args, typename vector<B>::iterator &it) {
+    void Call(int ID, vector<TUI_Childless *> *a, IArguments *i_args, typename vector<TUI_Childless *>::iterator &it) {
         for (it = a->begin(); it != a->end(); it++) {
             T *c = dynamic_cast<T *>(*it);
-            if (c) { (c->*m_method)(); };
+            if (c) {
+                (c->*m_method)();
+                c->sevents.Call(ID);
+            };
         }
     }
 
-    void Call(B b, IArguments *i_args) {
+    void Call(int ID, TUI_Childless *b, IArguments *i_args) {
+        T *c = dynamic_cast<T *>(b);
+        if (c) {
+            (c->*m_method)();
+            c->sevents.Call(ID);
+        };
+    }
+//##############################################################################
+
+    void SingleCall(int ID, list<TUI_Childless *> *a, IArguments *i_args) {
+        for (auto it = a->begin(); it != a->end(); it++) {
+            T *c = dynamic_cast<T *>(*it);
+            if (c) (c->*m_method)();
+        }
+    }
+
+    void
+    SingleCall(int ID, list<TUI_Childless *> *a, IArguments *i_args, typename list<TUI_Childless *>::iterator &it) {
+        for (it = a->begin(); it != a->end(); it++) {
+            T *c = dynamic_cast<T *>(*it);
+            if (c) (c->*m_method)();
+        }
+    }
+
+    void SingleCall(int ID, vector<TUI_Childless *> *a, IArguments *i_args) {
+        for (auto it = a->begin(); it != a->end(); it++) {
+            T *c = dynamic_cast<T *>(*it);
+            if (c) (c->*m_method)();
+        }
+    }
+
+    void
+    SingleCall(int ID, vector<TUI_Childless *> *a, IArguments *i_args, typename vector<TUI_Childless *>::iterator &it) {
+        for (it = a->begin(); it != a->end(); it++) {
+            T *c = dynamic_cast<T *>(*it);
+            if (c) (c->*m_method)();
+        }
+    }
+
+    void SingleCall(int ID, TUI_Childless *b, IArguments *i_args) {
         T *c = dynamic_cast<T *>(b);
         if (c) (c->*m_method)();
     }
@@ -67,8 +132,8 @@ public:
 
 // Спецификация для одного аргумента.
 
-template<class B, class T, class A1>
-class UIContainer<B, T, void (T::*)(A1)> : public IUIContainer {
+template<class T, class A1>
+class UIContainer<T, void (T::*)(A1)> : public IUIContainer {
 public:
 
     typedef void (T::*M)(A1);
@@ -81,7 +146,61 @@ private:
     T *m_class;
     M m_method;
 
-    void Call(list <B> *a, IArguments *i_args) {
+    void Call(int ID, list<TUI_Childless *> *a, IArguments *i_args) {
+        for (auto it = a->begin(); it != a->end(); it++) {
+            T *c = dynamic_cast<T *>(*it);
+            A *a = dynamic_cast< A * >( i_args );
+            if (c && a) {
+                (c->*m_method)(a->arg1);
+                c->sevents.Call(ID, a->arg1);
+            };
+        }
+    }
+
+    void Call(int ID, list<TUI_Childless *> *a, IArguments *i_args, typename list<TUI_Childless *>::iterator &it) {
+        for (it = a->begin(); it != a->end(); it++) {
+            T *c = dynamic_cast<T *>(*it);
+            A *a = dynamic_cast< A * >( i_args );
+            if (c && a) {
+                (c->*m_method)(a->arg1);
+                c->sevents.Call(ID, a->arg1);
+            };
+        }
+    }
+
+    void Call(int ID, vector<TUI_Childless *> *a, IArguments *i_args) {
+        for (auto it = a->begin(); it != a->end(); it++) {
+            T *c = dynamic_cast<T *>(*it);
+            A *a = dynamic_cast< A * >( i_args );
+            if (c && a) {
+                (c->*m_method)(a->arg1);
+                c->sevents.Call(ID, a->arg1);
+            };
+        }
+    }
+
+    void Call(int ID, vector<TUI_Childless *> *a, IArguments *i_args, typename vector<TUI_Childless *>::iterator &it) {
+        for (it = a->begin(); it != a->end(); it++) {
+            T *c = dynamic_cast<T *>(*it);
+            A *a = dynamic_cast< A * >( i_args );
+            if (c && a) {
+                (c->*m_method)(a->arg1);
+                c->sevents.Call(ID, a->arg1);
+            };
+        }
+    }
+
+    void Call(int ID, TUI_Childless *b, IArguments *i_args) {
+        T *c = dynamic_cast<T *>(b);
+        A *a = dynamic_cast< A * >( i_args );
+        if (c && a) {
+            (c->*m_method)(a->arg1);
+            c->sevents.Call(ID, a->arg1);
+        };
+    }
+//##########################################################################
+
+    void SingleCall(int ID, list<TUI_Childless *> *a, IArguments *i_args) {
         for (auto it = a->begin(); it != a->end(); it++) {
             T *c = dynamic_cast<T *>(*it);
             A *a = dynamic_cast< A * >( i_args );
@@ -89,7 +208,8 @@ private:
         }
     }
 
-    void Call(list <B> *a, IArguments *i_args, typename list<B>::iterator &it) {
+    void
+    SingleCall(int ID, list<TUI_Childless *> *a, IArguments *i_args, typename list<TUI_Childless *>::iterator &it) {
         for (it = a->begin(); it != a->end(); it++) {
             T *c = dynamic_cast<T *>(*it);
             A *a = dynamic_cast< A * >( i_args );
@@ -97,7 +217,7 @@ private:
         }
     }
 
-    void Call(vector <B> *a, IArguments *i_args) {
+    void SingleCall(int ID, vector<TUI_Childless *> *a, IArguments *i_args) {
         for (auto it = a->begin(); it != a->end(); it++) {
             T *c = dynamic_cast<T *>(*it);
             A *a = dynamic_cast< A * >( i_args );
@@ -105,7 +225,8 @@ private:
         }
     }
 
-    void Call(vector <B> *a, IArguments *i_args, typename vector<B>::iterator &it) {
+    void
+    SingleCall(int ID, vector<TUI_Childless *> *a, IArguments *i_args, typename vector<TUI_Childless *>::iterator &it) {
         for (it = a->begin(); it != a->end(); it++) {
             T *c = dynamic_cast<T *>(*it);
             A *a = dynamic_cast< A * >( i_args );
@@ -113,12 +234,11 @@ private:
         }
     }
 
-    void Call(B b, IArguments *i_args) {
+    void SingleCall(int ID, TUI_Childless *b, IArguments *i_args) {
         T *c = dynamic_cast<T *>(b);
         A *a = dynamic_cast< A * >( i_args );
         if (c && a) (c->*m_method)(a->arg1);
     }
-
 };
 
 class UIEvents {
@@ -153,7 +273,7 @@ public:
     void Connect(int ID, T *i_class, U i_method) {
         while (UIContainers.size() <= ID) UIContainers.push_back(0);
         if (UIContainers[ID] == 0) UIContainers[ID] = new vector<IUIContainer *>;
-        UIContainers[ID]->push_back(new UIContainer<TUI_Childless *, T, U>(i_class, i_method));
+        UIContainers[ID]->push_back(new UIContainer<T, U>(i_class, i_method));
     }
 
     // массовый вызов событий
@@ -162,16 +282,20 @@ public:
     {
         if ((ID >= UIContainers.size()) || (UIContainers[ID] == 0)) return;
         for (auto &v : *UIContainers[ID]) {
-            v->Call(array, (Arguments<> * )(0));
+            v->Call(ID, array, (Arguments<> * )(0));
         }
+
+        //cout << "Calling " << GetHookNameByID(ID) << endl;
     };
 
     void Call(int ID, typename vector<TUI_Childless *>::iterator it) // ИТЕРАЦИЯ!
     {
         if ((ID >= UIContainers.size()) || (UIContainers[ID] == 0)) return;
         for (auto &v : *UIContainers[ID]) {
-            v->Call(array, (Arguments<> * )(0), it);
+            v->Call(ID, array, (Arguments<> * )(0), it);
         }
+
+        //cout << "Calling " << GetHookNameByID(ID) << endl;
     };
 
     template<class T1>
@@ -182,6 +306,8 @@ public:
             Arguments <T1> args(i_arg1);
             v->Call(array, &args);
         }
+
+        //cout << "Calling " << GetHookNameByID(ID) << endl;
     };
 
     // Вызов события для частного объекта
@@ -190,8 +316,10 @@ public:
     {
         if ((ID >= UIContainers.size()) || (UIContainers[ID] == 0)) return;
         for (auto &v : *UIContainers[ID]) {
-            v->Call(b, (Arguments<> * )(0));
+            v->Call(ID, b, (Arguments<> * )(0));
         }
+
+        //cout << "Calling " << GetHookNameByID(ID) << endl;
     };
 
     template<class T1>
@@ -200,8 +328,10 @@ public:
         if ((ID >= UIContainers.size()) || (UIContainers[ID] == 0)) return;
         for (auto &v : *UIContainers[ID]) {
             Arguments <T1> args(i_arg1);
-            v->Call(b, &args);
+            v->Call(ID, b, &args);
         }
+
+        //cout << "Calling " << GetHookNameByID(ID) << endl;
     };
 
 
@@ -213,8 +343,10 @@ public:
     {
         if ((ID >= UIContainers.size()) || (UIContainers[ID] == 0)) return;
         for (auto &v : *UIContainers[ID]) {
-            v->Call(ar, (Arguments<> * )(0));
+            v->Call(ID, ar, (Arguments<> * )(0));
         }
+
+        //cout << "Calling " << GetHookNameByID(ID) << endl;
     };
 
     template<class T1>
@@ -223,10 +355,95 @@ public:
         if ((ID >= UIContainers.size()) || (UIContainers[ID] == 0)) return;
         for (auto &v : *UIContainers[ID]) {
             Arguments <T1> args(i_arg1);
-            v->Call(ar, &args);
+            v->Call(ID, ar, &args);
         }
-    };
-};
 
-UIEvents TUI_Childless::events;
-UIEvents &ui_events = TUI_Childless::events;
+        //cout << "Calling " << GetHookNameByID(ID) << endl;
+    };
+
+//################################# Single Calls!
+
+    void SingleCall(int ID) // один аргумент
+    {
+        if ((ID >= UIContainers.size()) || (UIContainers[ID] == 0)) return;
+        for (auto &v : *UIContainers[ID]) {
+            v->SingleCall(ID, array, (Arguments<> * )(0));
+        }
+
+        //cout << "Single Calling " << GetHookNameByID(ID) << endl;
+    };
+
+    void SingleCall(int ID, typename vector<TUI_Childless *>::iterator it) // ИТЕРАЦИЯ!
+    {
+        if ((ID >= UIContainers.size()) || (UIContainers[ID] == 0)) return;
+        for (auto &v : *UIContainers[ID]) {
+            v->SingleCall(ID, array, (Arguments<> * )(0), it);
+        }
+
+        //cout << "Single Calling " << GetHookNameByID(ID) << endl;
+    };
+
+    template<class T1>
+    void SingleCall(int ID, T1 i_arg1) // два аргумента
+    {
+        if ((ID >= UIContainers.size()) || (UIContainers[ID] == 0)) return;
+        for (auto &v : *UIContainers[ID]) {
+            Arguments <T1> args(i_arg1);
+            v->SingleCall(array, &args);
+        }
+
+        //cout << "Single Calling " << GetHookNameByID(ID) << endl;
+    };
+
+    // Вызов события для частного объекта
+
+    void SingleCall(TUI_Childless *b, int ID) // один аргумент
+    {
+        if ((ID >= UIContainers.size()) || (UIContainers[ID] == 0)) return;
+        for (auto &v : *UIContainers[ID]) {
+            v->SingleCall(ID, b, (Arguments<> * )(0));
+        }
+
+        //cout << "Single Calling " << GetHookNameByID(ID) << endl;
+    };
+
+    template<class T1>
+    void SingleCall(TUI_Childless *b, int ID, T1 i_arg1) // два аргумента
+    {
+        if ((ID >= UIContainers.size()) || (UIContainers[ID] == 0)) return;
+        for (auto &v : *UIContainers[ID]) {
+            Arguments <T1> args(i_arg1);
+            v->SingleCall(ID, b, &args);
+        }
+
+        //cout << "Single Calling " << GetHookNameByID(ID) << endl;
+    };
+
+
+// ############################ Для особого массива:
+
+    // массовый вызов событий
+
+    void SingleCall(list<TUI_Childless *> *ar, int ID) // один аргумент
+    {
+        if ((ID >= UIContainers.size()) || (UIContainers[ID] == 0)) return;
+        for (auto &v : *UIContainers[ID]) {
+            v->SingleCall(ID, ar, (Arguments<> * )(0));
+        }
+
+        //cout << "Single Calling " << GetHookNameByID(ID) << endl;
+    };
+
+    template<class T1>
+    void SingleCall(list<TUI_Childless *> *ar, int ID, T1 i_arg1) // два аргумента
+    {
+        if ((ID >= UIContainers.size()) || (UIContainers[ID] == 0)) return;
+        for (auto &v : *UIContainers[ID]) {
+            Arguments <T1> args(i_arg1);
+            v->SingleCall(ID, ar, &args);
+        }
+
+        //cout << "Single Calling " << GetHookNameByID(ID) << endl;
+    };
+
+};
